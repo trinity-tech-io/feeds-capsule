ee1d221f0614a03c8ccda9f474b0f823f22837e1
raozhiming <raozhiming@elastos.org>
2020-04-20 08:55:02 +0800

    add QRCodeModule in ServerInfoPageModule

--- src/app/pages/servers/server-info/server-info.module.ts
+++ src/app/pages/servers/server-info/server-info.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
-
+import { QRCodeModule } from 'angularx-qrcode';
 import { IonicModule } from '@ionic/angular';
 
 import { ServerInfoPage } from './server-info';
@@ -19,6 +19,7 @@ const routes: Routes = [
     CommonModule,
     FormsModule,
     IonicModule,
+    QRCodeModule,
     RouterModule.forChild(routes)
   ],
 
