82c15e5a3c3ac556943fecb069ad7a819a4624eb
snackwang <snackwang.petter@gmail.com>
2020-06-22 18:43:29 +0800

    Add empty page in notification UI

---
+++ src/app/components/source/source.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-source',
+  templateUrl: './source.component.html',
+  styleUrls: ['./source.component.scss'],
+})
+export class SourceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+  connectChannelSource(){
+    alert("connectChannelSource");
+  }
+}
