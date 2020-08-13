7e95b53c8442d066548070cc8c865aafcacd6e71
xuliting <xuliting_2008@yeah.net>
2020-08-13 18:33:41 +0800

    update the translation

--- src/assets/i18n/fr.ts
+++ src/assets/i18n/fr.ts
@@ -291,7 +291,7 @@ export const fr = {
     "DisclaimerPage":{
         "disclaimer":"Disclaimer",
         "introduce":"Text for the disclaimer:",
-        "introduce1":"By clicking yes you hereby accept and agree to our Terms below.",
+        "introduce1":"By clicking \“Agree\” you hereby accept and agree to our Terms below.",
         "introduce2":"Feeds is owned and operated by Trinity Tech Co., Ltd. and its affiliates (“Trinity”, “we”, or “us”), and you agree to abide by the specific terms for the Feeds application below (collectively our “Terms”).",
         "introduce3":"By using our Feeds application, its content, and services (the “App”), you agree to be solely responsible for your use of the App and any content you post and provide, including compliance with applicable laws, rules, and regulations. You should only post and provide content that you are comfortable sharing with others. You must be at least 13 years old to use the App or no longer considered to be a minor in your jurisdiction (ie. state, providence or country).  We do not endorse, support, represent or guarantee the completeness, truthfulness, accuracy, or reliability of any content on the App. You hereby acknowledge that you may be exposed to harmful, inaccurate or inappropriate content while using the App."
     },
