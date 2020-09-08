2e7c63481134e19a91a165e05ef7be60dcc3f6a1
songjiazhun <869077613@qq.com>
2020-09-08 10:46:22 +0800

    add editcomment page

---
+++ src/app/pages/editcomment/editcomment.page.spec.ts
@@ -0,0 +1,27 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditcommentPage } from './editcomment.page';
+
+describe('EditcommentPage', () => {
+  let component: EditcommentPage;
+  let fixture: ComponentFixture<EditcommentPage>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EditcommentPage ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EditcommentPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
