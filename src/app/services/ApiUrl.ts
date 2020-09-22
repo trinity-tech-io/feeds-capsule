c2aa54998bc5f2e40f417fc14241ef39354f270d
snackwang <snackwang.petter@gmail.com>
2020-09-22 21:34:30 +0800

    Update discover feed url

--- src/app/services/ApiUrl.ts
+++ src/app/services/ApiUrl.ts
@@ -1,6 +1,6 @@
 export  class ApiUrl {
   /**后台服务*/
-  public static SERVER:string = 'https://triniy-tech.io/feeds/api/v1';
+  public static SERVER:string = 'https://www.trinity-tech.io/feeds/api/v1';
   /**register*/
   public static register:string = ApiUrl.SERVER +'/register';
 
