1a0940ddc591e7593eeb9bc055e22f517e3e945c
songjiazhun <869077613@qq.com>
2020-10-10 16:06:16 +0800

    add video record demon

---
+++ src/app/pages/videorecord/videorecord.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
+
+import { VideorecordPage } from './videorecord.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: VideorecordPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    TranslateModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [VideorecordPage]
+})
+export class VideorecordPageModule {}
