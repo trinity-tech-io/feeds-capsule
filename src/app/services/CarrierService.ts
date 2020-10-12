ca477746759b5ec41910a586d54671ce5d4aa772
songjiazhun <869077613@qq.com>
2020-10-12 10:33:32 +0800

    fix compile bug

--- src/app/services/CarrierService.ts
+++ src/app/services/CarrierService.ts
@@ -29,7 +29,17 @@ let UserInfo: CarrierPlugin.UserInfo;
 let carrierInst: CarrierPlugin.Carrier;
 let eventBus = null;
 
-const createOption = {
+// var connectStatus ;
+
+// const bootstrapsOpts = [
+//     { ipv4: "13.58.208.50", port: "33445", publicKey: "89vny8MrKdDKs7Uta9RdVmspPjnRMdwMmaiEW27pZ7gh" },
+//     { ipv4: "18.216.102.47", port: "33445", publicKey: "G5z8MqiNDFTadFUPfMdYsYtkUDbX5mNCMVHMZtsCnFeb" },
+//     { ipv4: "18.216.6.197", port: "33445", publicKey: "H8sqhRrQuJZ6iLtP2wanxt4LzdNrN2NNFnpPdq1uJ9n2" },
+//     { ipv4: "52.83.171.135", port: "33445", publicKey: "5tuHgK1Q4CYf4K5PutsEPK5E3Z7cbtEBdx7LwmdzqXHL" },
+//     { ipv4: "52.83.191.228", port: "33445", publicKey: "3khtxZo89SBScAMaHhTvD68pPHiKxgZT6hTCSZZVgNEm" }
+// ];
+
+const createOption:any = {
     udpEnabled: true,
     persistentLocation: ".data",
     binaryUsed: true,
