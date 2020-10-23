31f7baf58e86710266f3575d2d1af29b20b99b7f
snackwang <snackwang.petter@gmail.com>
2020-10-23 10:20:25 +0800

    Decode head data on stream receive data

--- src/app/services/SessionService.ts
+++ src/app/services/SessionService.ts
@@ -4,6 +4,8 @@ import { NativeService } from 'src/app/services/NativeService';
 import { TranslateService } from '@ngx-translate/core';
 import { CarrierService } from 'src/app/services/CarrierService';
 import { SerializeDataService } from 'src/app/services/SerializeDataService';
+import { join } from 'path';
+// let base64 = require('cordova/base64');
 
 type WorkedSession = {
     nodeId      : string,
@@ -12,7 +14,27 @@ type WorkedSession = {
     isStart     : boolean,
     sdp         : string
 }
+
+type CachedData = {
+    nodeId          : string,
+    data            : Uint8Array,
+    pointer         : number,
+    headSize        : number,
+    bodySize        : number,
+    state           : DecodeState
+}
+
+const enum DecodeState{
+    prepare ,
+    decodeHead,
+    decodeBody,
+    finish
+}
+
 const pow32 = 0x100000000;   // 2^32
+const magicNumber: number = 0x0000A5202008275A;
+const version: number = 10000;
+
 const enum WorkedState {
     sessionInitialized = "sessionInitialized",
     streamInitialized = "streamInitialized",
@@ -25,8 +47,9 @@ const enum WorkedState {
 
 let mCarrierService;
 let eventBus:Events = null;
-let workedSessions:{[nodeId:string]: WorkedSession} = {}; 
-let cacheData = [];
+let testSum = 0;
+let workedSessions: {[nodeId:string]: WorkedSession} = {}; 
+let cacheData: {[nodeId:string]: CachedData} = {};
 let mSerializeDataService:SerializeDataService;
 @Injectable()
 export class SessionService {
@@ -43,7 +66,6 @@ export class SessionService {
     }
   
     createSession(nodeId: string, onSuccess:(session: CarrierPlugin.Session, stream: CarrierPlugin.Stream)=>void, onError?:(err: string)=>void){
-        console.log("nodeId =="+nodeId)
         this.carrierService.newSession(nodeId,(mSession)=>{
             console.log("newSession success");
             if (workedSessions)
@@ -70,10 +92,10 @@ export class SessionService {
                             "closed",
                             "failed"
                         ];
-                        console.log("111111111111111");
+
                         var msg = "Stream [" + event.stream.id + "] state changed to: " + state_name[event.state];
                         console.log(msg);
-                        eventBus.publish("stream:onStateChangedCallback",(event.stream.id, state_name[event.state]));
+                        eventBus.publish("stream:onStateChangedCallback", nodeId, state_name[event.state]);
 
                         if (CarrierPlugin.StreamState.INITIALIZED == event.state){
                             console.log("aaaaaaaaaaaaaaa");
@@ -134,7 +156,84 @@ export class SessionService {
                             
                         }
                     },
-                    onStreamData: this.onStreamDataCallback
+                    onStreamData: function(event: any) {
+                        let tmpData: Uint8Array = event.data;
+                        console.log("---onStreamData---"+tmpData.length);
+                        if (cacheData[nodeId] == undefined){
+                            cacheData[nodeId] = {
+                                nodeId          : nodeId,
+                                data            : new Uint8Array(tmpData.length),
+                                pointer         : 0,
+                                headSize        : 0,
+                                bodySize        : 0,
+                                state           : DecodeState.prepare
+                            }
+                            cacheData[nodeId].data.set(tmpData,0);
+                        }else{
+                            let cache: Uint8Array = cacheData[nodeId].data;
+                            cacheData[nodeId].data = new Uint8Array(tmpData.length+cache.length);
+                            cacheData[nodeId].data.set(cache,0);
+                            cacheData[nodeId].data.set(tmpData,cache.length);
+                        }
+
+                        let dataLength = cacheData[nodeId].data.length;
+                        switch(cacheData[nodeId].state){
+                            case DecodeState.prepare:
+                                for (let index = 0; index < dataLength - 23; index++) {
+                                    let decodeMagicNumData = cacheData[nodeId].data.subarray(index,index+8);
+                                    let decodeMagicNumber = decodeNum(decodeMagicNumData,8);
+                                    cacheData[nodeId].pointer = index;
+                                    console.log("decodeMagicNumber="+decodeMagicNumber);
+        
+                                    if( decodeMagicNumber != magicNumber )
+                                        continue;
+
+                                    let decodeVersionData = cacheData[nodeId].data.subarray(index+8,index+12);
+                                    let decodeVersion = decodeNum(decodeVersionData,4);
+                                    if (decodeVersion != version)
+                                        continue;
+
+                                    let decodeHeadSizeData = cacheData[nodeId].data.subarray(index+12,index+16);
+                                    let decodeHeadSize = decodeNum(decodeHeadSizeData,4);
+
+                                    let decodeBodySizeData = cacheData[nodeId].data.subarray(index+16,index+24);
+                                    let decodeBodySize = decodeNum(decodeBodySizeData,8);
+
+                                    cacheData[nodeId].headSize = decodeHeadSize;
+                                    cacheData[nodeId].bodySize = decodeBodySize;
+                                    cacheData[nodeId].state = DecodeState.decodeHead;
+                                    cacheData[nodeId].pointer = cacheData[nodeId].pointer + 24;
+                                    break;
+                                }
+                                
+                                if (!decodeHeadData(nodeId, dataLength)){
+                                    break;
+                                }
+                                    
+                                if (!decodeBodyData(nodeId, dataLength)){
+                                    break;
+                                }
+
+                                break;
+                            case DecodeState.decodeHead:
+                                if (!decodeHeadData(nodeId, dataLength)){
+                                    break;
+                                }
+                                    
+                                if (!decodeBodyData(nodeId, dataLength)){
+                                    break;
+                                }
+                                
+                                break;
+                            case DecodeState.decodeBody:
+                                if (!decodeBodyData(nodeId, dataLength)){
+                                    break;
+                                }
+                                break;
+                        }
+
+                        combineData(nodeId, dataLength);
+                    }
                 }, 
                 (mStream)=>{
                     workedSessions[nodeId].stream = mStream;
@@ -193,33 +292,35 @@ export class SessionService {
     }
 
     streamAddMagicNum(nodeId: string){
-        let magicNumber: number = 0x0000A5202008275A;
         let bytesData = encodeNum(magicNumber,8);
+        // let base64 = uint8arrayToBase64(bytesData);
         this.streamAddData(nodeId, bytesData);
     }
 
     streamAddVersion(nodeId: string){
-        let version: number = 10000;
-        let byteData = encodeNum(version,4);
-        this.streamAddData(nodeId, byteData);
+        
+        let bytesData = encodeNum(version,4);
+        // let base64 = uint8arrayToBase64(bytesData);
+        this.streamAddData(nodeId, bytesData);
     }
 
     streamAddRequestHeadSize(nodeId: string, headSize: number){
-        let byteData = encodeNum(headSize,4);
-        this.streamAddData(nodeId, byteData);
+        let bytesData = encodeNum(headSize,4);
+        // let base64 = uint8arrayToBase64(bytesData);
+        this.streamAddData(nodeId, bytesData);
     }
 
     streamAddRequestBodySize(nodeId: string, bodySize: number){
-        let byteData = encodeNum(bodySize,4);
-        this.streamAddData(nodeId, byteData);
+        let bytesData = encodeNum(bodySize,8);
+        // let base64 = uint8arrayToBase64(bytesData);
+        this.streamAddData(nodeId, bytesData);
     }
 
     streamAddRequest(nodeId: string, request: any){
-        let data = this.serializeDataService.encodeData(request);
-        this.streamAddData(nodeId, data);
+        this.streamAddData(nodeId, request);
     }
 
-    addHeadData(nodeId: string, requestSize: number, bodySize: number){
+    addHeader(nodeId: string, requestSize: number, bodySize: number){
         this.streamAddMagicNum(nodeId);
         this.streamAddVersion(nodeId);
         this.streamAddRequestHeadSize(nodeId, requestSize);
@@ -262,7 +363,11 @@ export class SessionService {
 
     }
 
-    streamAddData(nodeId: string, data: any){
+    stramAddBase64Data(nodeId: string, data: any){
+
+    }
+    
+    streamAddData(nodeId: string, data: Uint8Array){
         if (workedSessions == null ||
             workedSessions == undefined ||
             workedSessions[nodeId] == undefined){
@@ -271,40 +376,59 @@ export class SessionService {
         }
 
         let stream = workedSessions[nodeId].stream;
-        console.log("==========streamAddData============");
-        stream.write(data,(bytesSent)=>{
+
+        testSum += data.length
+        console.log("==========streamAddData============testSum = "+testSum);
+        console.log("==========streamAddData============data.length = "+data.length);
+        
+
+        // console.log("==========streamAddData============"+JSON.stringify(data));
+        let base64 = uint8arrayToBase64(data);
+
+        stream.write(base64,(bytesSent)=>{
             console.log("stream write success");
             // console.log("bytesSent===>"+JSON.stringify(bytesSent));
         },(err)=>{
-            console.log("stream write error");
+            console.log("stream write error ==>"+err);
         });
     }
     
     onStreamDataCallback(event) {
-        console.log("Stream [" + event.stream.id + "] received data [" + event.data + "]");
-        console.log("data = "+uint8ArrayToString(event.data));
 
-        cacheData.push(event.data);
-        console.log("cacheData ==>"+cacheData.length);
-        console.log("cacheData ==>"+JSON.stringify(cacheData));
-        let magicNumberData = cacheData.slice(0,8);
-        console.log("magicNumberData = "+JSON.stringify(magicNumberData));
-        let magicNumber = mSerializeDataService.decodeData(event.data);
-        console.log("magicNumber = "+magicNumber);
-        if (cacheData.length>=12){
-            let magicNumberData = cacheData.slice(0,8);
-            console.log("magicNumberData = "+JSON.stringify(magicNumberData));
-            let magicNumber = mSerializeDataService.decodeData(magicNumberData);
-            console.log("magicNumber = "+magicNumber);
+
+
+
+        // let tmpData: Uint8Array = event.data;
+
+        // cacheData[nodeId]
+
+        // if(catchData)
+        
+        // console.log("Stream [" + event.stream.id + "] received data [" + event.data + "]");
+        // console.log("data = "+uint8ArrayToString(event.data));
+
+
+        // cacheData.push(event.data);
+        // console.log("cacheData ==>"+cacheData.length);
+        // console.log("cacheData ==>"+JSON.stringify(cacheData));
+        // let magicNumberData = cacheData.slice(0,8);
+        // console.log("magicNumberData = "+JSON.stringify(magicNumberData));
+        // let magicNumber = mSerializeDataService.decodeData(event.data);
+        // console.log("magicNumber = "+magicNumber);
+        // if (cacheData.length>=12){
+        //     let magicNumberData = cacheData.slice(0,8);
+        //     console.log("magicNumberData = "+JSON.stringify(magicNumberData));
+        //     let magicNumber = mSerializeDataService.decodeData(magicNumberData);
+        //     console.log("magicNumber = "+magicNumber);
     
-            let versionData = cacheData.slice(9,12);
-            console.log("versionData = "+JSON.stringify(versionData));
-            let version = mSerializeDataService.decodeData(versionData);
-            console.log("version = "+version);
+        //     let versionData = cacheData.slice(9,12);
+        //     console.log("versionData = "+JSON.stringify(versionData));
+        //     let version = mSerializeDataService.decodeData(versionData);
+        //     console.log("version = "+version);
     
-            console.log("data type= "+typeof(event.data));
+        //     console.log("data type= "+typeof(event.data));
             
-        }
+        // }
 
         eventBus.publish("stream:onStreamDataCallback",(event.stream.id, event.data));
         
@@ -432,6 +556,28 @@ function decodeNum(data: Uint8Array, size: number) {
     }
     return value;
 }
+// function uint8ToBase64 (rawData) {
+//     var numBytes = rawData.byteLength;
+//     var output = '';
+//     var segment;
+//     var table = b64_12bitTable();
+//     for (var i = 0; i < numBytes - 2; i += 3) {
+//         segment = (rawData[i] << 16) + (rawData[i + 1] << 8) + rawData[i + 2];
+//         output += table[segment >> 12];
+//         output += table[segment & 0xfff];
+//     }
+//     if (numBytes - i === 2) {
+//         segment = (rawData[i] << 16) + (rawData[i + 1] << 8);
+//         output += table[segment >> 12];
+//         output += b64_6bit[(segment & 0xfff) >> 6];
+//         output += '=';
+//     } else if (numBytes - i === 1) {
+//         segment = (rawData[i] << 16);
+//         output += table[segment >> 12];
+//         output += '==';
+//     }
+//     return output;
+// }
 
 function uint8ArrayToString(data: Uint8Array): string{
     var dataString = "";
@@ -440,4 +586,116 @@ function uint8ArrayToString(data: Uint8Array): string{
     }
    
     return dataString
+}
+
+function uint8arrayToBase64(u8Arr: Uint8Array): any{
+    let CHUNK_SIZE = 0x8000; 
+    let index = 0;
+    let length = u8Arr.length;
+    let result = '';
+    let slice;
+    while (index < length) {
+        slice = u8Arr.subarray(index, Math.min(index + CHUNK_SIZE, length));
+        result += String.fromCharCode.apply(null, slice);
+        index += CHUNK_SIZE;
+    }
+    return btoa(result);
+}
+
+function base64ToUint8Array(base64String: string): Uint8Array {
+    let padding = '='.repeat((4 - base64String.length % 4) % 4);
+    let base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    let rawData = window.atob(base64);
+    let outputArray = new Uint8Array(rawData.length);
+
+    for (var i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
+
+function decodeHeadData(nodeId: string, cacheDataLength: number): boolean{
+    if (cacheData == null || cacheData == undefined || cacheData[nodeId] == undefined)
+        return false;
+
+    if (cacheData[nodeId].state != DecodeState.decodeHead)
+        return false;
+
+
+    
+    let pointer = cacheData[nodeId].pointer;
+    let headSize = cacheData[nodeId].headSize;
+    let remainLength = cacheDataLength - pointer;
+
+    if (remainLength < headSize)
+        return false;
+    console.log("--------cacheDataLength--------"+cacheDataLength);
+
+    console.log("--------remainLength--------"+remainLength);
+    console.log("--------pointer--------"+pointer);
+    console.log("--------headSize--------"+headSize);
+    let headResponse = cacheData[nodeId].data.subarray(pointer,pointer + headSize);
+    console.log("--------headResponse--------"+JSON.stringify(headResponse));
+    console.log("--------response--------"+headResponse.length);
+
+    let test = uint8ArrayToString(headResponse);
+    console.log("----------response body----------"+JSON.stringify(test));
+
+    if (mSerializeDataService == undefined){
+        console.log("----------undefined----------");
+    }else{
+        console.log("----------nononono----------");
+    }
+    let response = mSerializeDataService.decodeData(headResponse);
+    console.log("----------response body----------"+JSON.stringify(response));
+
+    
+    cacheData[nodeId].state = DecodeState.decodeBody;
+    cacheData[nodeId].pointer = cacheData[nodeId].pointer + headSize;
+    return true;
+}
+
+function decodeBodyData(nodeId: string, cacheDataLength: number): boolean{
+    if (cacheData == null || cacheData == undefined || cacheData[nodeId] == undefined)
+        return false;
+
+    if (cacheData[nodeId].state != DecodeState.decodeBody)
+        return false;
+
+    let headSize = cacheData[nodeId].headSize;
+    let pointer = cacheData[nodeId].pointer
+    let bodySize = cacheData[nodeId].bodySize;
+    let remainLength = cacheDataLength - pointer;
+
+    if (remainLength < bodySize)
+        return false;
+
+    let body = cacheData[nodeId].data.subarray(pointer, pointer + bodySize);
+    console.log("----------body----------"+body.length);
+    
+
+    cacheData[nodeId].state = DecodeState.finish;
+    cacheData[nodeId].pointer = cacheData[nodeId].pointer + bodySize;
+    return true;
+}
+
+function combineData(nodeId: string, cacheDataLength: number){
+    if (cacheData == null || cacheData == undefined || cacheData[nodeId] == undefined)
+        return false;
+
+    if (cacheData[nodeId].state == DecodeState.finish)
+        return false;
+
+    let pointer = cacheData[nodeId].pointer;
+    let remainData = cacheData[nodeId].data.subarray(pointer, cacheDataLength);
+
+    console.log("remainData =="+remainData.length);
+    console.log("pointer =="+pointer);
+    cacheData[nodeId].data = new Uint8Array(remainData.length);
+    cacheData[nodeId].data.set(remainData,0);
+    cacheData[nodeId].pointer = 0;
+    return true;
 }
