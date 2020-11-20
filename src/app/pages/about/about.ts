9df407752ae588291e6193ce2f2d0946d365db2e
snackwang <snackwang.petter@gmail.com>
2020-11-20 22:42:02 +0800

    Update version to 1.3.1

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -15,7 +15,7 @@ declare let titleBarManager: TitleBarPlugin.TitleBarManager;
 export class AboutPage implements OnInit {
 
   public connectionStatus = 1;
-  public version = "v1.3.0";
+  public version = "v1.3.1";
   public curlangule = "";
   constructor(
     private zone: NgZone,
