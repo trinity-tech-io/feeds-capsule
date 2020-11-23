4985faabeaae98db8895535ef34c77bd38b42928
snackwang <snackwang.petter@gmail.com>
2020-11-23 14:27:39 +0800

    CU-59pv26 Add LogUtils

--- src/app/services/LogUtils.ts
+++ src/app/services/LogUtils.ts
@@ -1,17 +1,63 @@
 import { Injectable } from '@angular/core';
 
+enum LogLevel {
+    NONE,
+    ERROR,
+    WARN,
+    INFO,
+    DEBUG,
+}
+
+let TAG = "Feeds";
 @Injectable()
 export class LogUtils {
-    private isDebug = true;
-    private tag = "Feeds>>"
+    private level:LogLevel = LogLevel.WARN;
+    
     constructor() {}
-    log(tag:string, msg: string){
-        if(this.isDebug)
-            console.log(msg);
+    log(msg: string, tag: string, level: LogLevel){
+        if (!this.checkLogLevel(level)){
+            return ;
+        }
+        console.log(tag+"::"+msg);
     }
 
-    logd(msg: string){
-        if(this.isDebug)
-            console.log(this.tag+msg);
+    logd(msg: string, tag:string = TAG){
+        this.log(msg, tag, LogLevel.DEBUG);
+    }
+
+    logi(msg: string, tag: string = TAG){
+        this.log(msg, tag, LogLevel.INFO);
+    }
+
+    logw(msg: string, tag: string = TAG){
+        this.log(msg, tag, LogLevel.WARN);
+    }
+
+    loge(msg: string, tag: string = TAG){
+        this.log(msg, tag, LogLevel.ERROR);
+    }
+
+    checkLogLevel(level: LogLevel): boolean{
+        if (this.level < level)
+            return false;
+        return true;
+    }
+
+    setLogLevel(level: LogLevel){
+        if (level< LogLevel.NONE){
+            this.level = LogLevel.NONE;
+            return;
+        }
+
+        if (level > LogLevel.DEBUG){
+            this.level = LogLevel.DEBUG;
+            return;
+        }
+
+        this.level = level;
+    }
+
+    getLogLevel():LogLevel{
+        return this.level;
     }
 }
