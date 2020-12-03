4ce0c8c6f09b5fe5e07c84257c62481c2d570975
stiartsly <stiartsly@gmail.com>
2020-12-03 16:01:51 +0800

    CU-3xm1zj polish the description for 'releaseToRefresh' and 'beingReresh'

--- src/assets/i18n/zh.ts
+++ src/assets/i18n/zh.ts
@@ -2,8 +2,8 @@ export const zh = {
     "common":{
         "followers":"关注",
         "allempty":"空空如也",
-        "pullToRefresh":"释放更新",
-        "Refreshing":"刷新中...",
+        "releaseToRefresh":"释放刷新内容",
+        "beingRefresh":"刷新中...",
         "cancel":"取消",
         "ok":"确定",
         "confirm":"确认",
