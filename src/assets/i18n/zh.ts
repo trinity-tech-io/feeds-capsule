446458be356a73a7e5eea370db0a858375128a8e
songjiazhun <869077613@qq.com>
2020-12-03 16:09:48 +0800

    CU-grvd4m - video(15s)

--- src/assets/i18n/zh.ts
+++ src/assets/i18n/zh.ts
@@ -72,7 +72,7 @@ export const zh = {
         "followed": "关注",
         "unfollowed": "取消关注",
         "infoObtaining": "信息获取中",
-        "filevideodes":"此视频文件超过最大限制",
+        "filevideodes":"您的视频文件播放长度超出当前限制 (15秒）",
         "videoloading":"视频加载中",
         "videocompression":"视频压缩中",
         "video":"视频",
