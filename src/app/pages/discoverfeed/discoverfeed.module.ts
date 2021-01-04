d24d800efe3ef61d91aed2a8dc5339782d5da3d3
songjiazhun <869077613@qq.com>
2021-01-04 13:47:12 +0800

    add discoverfeed

---
+++ src/app/pages/discoverfeed/discoverfeed.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { IonicModule } from '@ionic/angular';
+
+import { DiscoverfeedPage } from './discoverfeed.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DiscoverfeedPage
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    TranslateModule,
+    IonicModule,
+    RouterModule.forChild(routes)
+  ],
+  declarations: [DiscoverfeedPage]
+})
+export class DiscoverfeedPageModule {}
