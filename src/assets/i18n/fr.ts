812a318a9f60eb890de9dfeb8eca0c54ba022cb9
songjiazhun <869077613@qq.com>
2021-02-04 17:12:10 +0800

    update i18n

--- src/assets/i18n/fr.ts
+++ src/assets/i18n/fr.ts
@@ -91,7 +91,7 @@ export const fr = {
         "unfollow":"Unfollow",
         "adding":"Adding",
         "feedsAlreadyAdded":"This feed has already been added",
-        "notFollowYet":"Not follow yet",
+        "notFollowYet":"Follow",
         "httperror":"Service exception, try again later"
     },
     "app":{
