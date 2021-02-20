68e2f86bb89f8e7c9323adb90bfc8f78c9a957ad
songjiazhun <869077613@qq.com>
2021-02-20 10:35:52 +0800

    CU-6kvy2x - fix pages issuse

--- src/app/pages/feedinfo/feedinfo.page.ts
+++ src/app/pages/feedinfo/feedinfo.page.ts
@@ -3,15 +3,10 @@ import { TranslateService } from "@ngx-translate/core";
 import { Events} from '@ionic/angular';
 import { ThemeService } from '../../services/theme.service';
 import { FeedService } from '../../services/FeedService';
-import { ActivatedRoute } from '@angular/router';
 import { NativeService } from '../../services/NativeService';
-import { HttpService } from '../../services/HttpService';
-import { ApiUrl } from '../../services/ApiUrl';
-import { UtilService } from 'src/app/services/utilService';
-import { StorageService } from '../../services/StorageService';
-import { MenuService } from 'src/app/services/MenuService';
+import { MenuService } from '../../services/MenuService';
 import { PopoverController} from '@ionic/angular';
-import { PaypromptComponent } from 'src/app/components/payprompt/payprompt.component'
+import { PaypromptComponent } from '../../components/payprompt/payprompt.component'
 
 import * as _ from 'lodash';
 declare let titleBarManager: TitleBarPlugin.TitleBarManager;
@@ -44,11 +39,7 @@ export class FeedinfoPage implements OnInit {
   public oldChannelAvatar:string = "";
   public serverInfo:any = {};
   public feedsUrl: string = null;
-  public isOwer:number = 2;
-  public isPublic:string = "";
   public qrcodeString:string = null;
-  public feedPublicStatus:any = {};
-  public nodeStatus:any = {};
   public severVersion:string ="";
   public elaAddress:string ="";
   public developerMode:boolean =  false;
@@ -59,14 +50,11 @@ export class FeedinfoPage implements OnInit {
   constructor(
     private popoverController:PopoverController,
     private feedService: FeedService,
-    public activatedRoute:ActivatedRoute,
     public theme:ThemeService,
     private translate:TranslateService,
     private events: Events,
     private native: NativeService,
     private zone:NgZone,
-    private httpService: HttpService,
-    public storageService:StorageService,
     private menuService: MenuService
   ) {
     }
@@ -80,14 +68,9 @@ export class FeedinfoPage implements OnInit {
       this.severVersion = this.feedService.getServerVersionByNodeId(this.nodeId) || '<1.3.0(Outdated)';
       this.elaAddress = this.serverInfo["elaAddress"] || this.translate.instant('DIDdata.Notprovided');
       let feedsUrl = this.serverInfo['feedsUrl'] || null;
-      let did = this.serverInfo['did'] || "";
-      this.isOwer = this.checkIsMine(did);
       this.channelId = channelInfo["channelId"] || "";
       this.feedsUrl = feedsUrl+"/"+this.channelId;
       this.name = channelInfo["name"] || "";
-      this.feedPublicStatus = this.feedService.getFeedPublicStatus();
-      let feedsUrlHash =UtilService.SHA256(this.feedsUrl);
-      this.isPublic = this.feedPublicStatus[feedsUrlHash] || "";
       this.qrcodeString = this.feedsUrl+"#"+encodeURIComponent(this.name);
       this.des = channelInfo["des"] || "";
       this.oldChannelAvatar = this.feedService.getProfileIamge();
@@ -102,7 +85,6 @@ export class FeedinfoPage implements OnInit {
   ionViewWillEnter() {
     this.developerMode = this.feedService.getDeveloperMode();
     this.initTitle();
-    this.initnodeStatus(this.nodeId);
     this.connectionStatus = this.feedService.getConnectionStatus();
     this.channelAvatar = this.feedService.getProfileIamge();
     this.avatar = this.feedService.parseChannelAvatar(this.channelAvatar);
@@ -129,12 +111,6 @@ export class FeedinfoPage implements OnInit {
 
     this.events.subscribe(FeedsEvent.PublishType.editChannel,()=>{
       this.clickEdit();
-    });
-
-    this.events.subscribe(FeedsEvent.PublishType.friendConnectionChanged, (nodeId, status)=>{
-      this.zone.run(()=>{
-        this.nodeStatus[nodeId] = status;
-      });
     });
 
     this.events.subscribe(FeedsEvent.PublishType.subscribeFinish, (nodeId, channelId)=> {
@@ -185,7 +161,6 @@ export class FeedinfoPage implements OnInit {
     this.events.unsubscribe(FeedsEvent.PublishType.connectionChanged);
     this.events.unsubscribe(FeedsEvent.PublishType.editFeedInfoFinish);
     this.events.unsubscribe(FeedsEvent.PublishType.rpcRequestError);
-    this.events.unsubscribe(FeedsEvent.PublishType.friendConnectionChanged);
   }
 
   profileimage(){
@@ -276,55 +251,6 @@ export class FeedinfoPage implements OnInit {
     return 1;
   }
 
-  clickPublic(){
-    if(this.isPublic === ""){
-      this.publicFeeds();
-   }else{
-      this.unPublicFeeds();
-   }
-  }
-
-  publicFeeds(){
-    let channel = this.feedService.getChannelFromId(this.nodeId, this.channelId);
-    let followers = channel.subscribers;
-    let feedsUrlHash = UtilService.SHA256(this.feedsUrl);
-    let obj = {
-      "did":this.serverInfo['did'],
-      "name":this.name,
-      "description":this.des,
-      "url":this.feedsUrl,
-      "feedsUrlHash":feedsUrlHash,
-      "feedsAvatar":this.channelAvatar,
-      "followers":followers,
-      "ownerName":this.serverInfo["owner"],
-      "nodeId":this.nodeId,
-      "ownerDid":channel["owner_did"]
-    };
-
-    this.httpService.ajaxPost(ApiUrl.register,obj).then((result)=>{
-      if(result["code"] === 200){
-          this.isPublic = "1";
-          this.feedPublicStatus[feedsUrlHash] = "1";
-          this.feedService.setFeedPublicStatus(this.feedPublicStatus);
-          this.storageService.set("feeds.feedPublicStatus",JSON.stringify(this.feedPublicStatus));
-          this.native.toast_trans("ServerInfoPage.publicTip");
-      }
-    });
-  }
-
-  unPublicFeeds(){
-    let feedsUrlHash = UtilService.SHA256(this.feedsUrl);
-    this.httpService.ajaxGet(ApiUrl.remove+"?feedsUrlHash="+feedsUrlHash).then((result)=>{
-      if(result["code"] === 200){
-          this.isPublic = "";
-          this.feedPublicStatus =_.omit(this.feedPublicStatus,[feedsUrlHash]);
-          this.feedService.setFeedPublicStatus(this.feedPublicStatus);
-          this.storageService.set("feeds.feedPublicStatus",JSON.stringify(this.feedPublicStatus));
-          this.native.toast_trans("ServerInfoPage.unpublicTip");
-      }
-    });
-  }
-
   clickEdit(){
     if(this.feedService.getConnectionStatus() != 0){
       this.native.toastWarn('common.connectionError');
@@ -339,14 +265,6 @@ export class FeedinfoPage implements OnInit {
     this.native.go("/eidtchannel");
   }
 
-  initnodeStatus(nodeId:string) {
-    let status = this.checkServerStatus(nodeId);
-    this.nodeStatus[nodeId] = status;
-  }
-
-  checkServerStatus(nodeId: string){
-    return this.feedService.getServerStatusFromId(nodeId);
-  }
 
   tip(){
     let server = this.feedService.getServerbyNodeId(this.nodeId)||undefined;
