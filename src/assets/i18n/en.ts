d046a9138f4cc9ec925bdeb1d288bd2c78ca0bf7
songjiazhun <869077613@qq.com>
2021-03-12 14:22:14 +0800

    update i18n

--- src/assets/i18n/en.ts
+++ src/assets/i18n/en.ts
@@ -228,7 +228,7 @@ export const en = {
         "name": "Name",
         "did": "DID",
         // "gender": "Gender",
-        "telephone": "PHONE NUMBER",
+        "telephone": "Phone Number",
         "email": "Email",
         "location": "Location",
         "carrieruserid": "Carrier UserId"
