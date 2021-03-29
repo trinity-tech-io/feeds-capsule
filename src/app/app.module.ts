1d095e734635fd115bf1bfb79783b44243d42617
snackwang <snackwang.petter@gmail.com>
2021-03-29 10:09:13 +0800

    Update version to 1.5.4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -66,7 +66,7 @@ import * as Sentry from "@sentry/browser";
 
 Sentry.init({
   dsn: "https://4196003a1c864f5798dd2be18be5cb48@o339076.ingest.sentry.io/5524842",
-  release: "1.5.3",
+  release: "1.5.4",
   integrations: [
     new RewriteFrames(),
   ]
