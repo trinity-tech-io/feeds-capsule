01d1ffe80b54bef68548c3dbd9d13b831c2b3383
songjiazhun <869077613@qq.com>
2021-04-03 21:50:34 +0800

    CU-r56zvw - fix createnewfeed page style

--- src/app/components/tipdialog/tipdialog.component.ts
+++ src/app/components/tipdialog/tipdialog.component.ts
@@ -11,7 +11,7 @@ export class TipdialogComponent implements OnInit {
   public did:string = "";
   public feedName:string = "";
   public feedDesc:string = "";
-  public feedPublicStatus:string ="";
+  public feedPublicStatus:boolean = true;
   constructor(
     public theme: ThemeService,
     private navParams: NavParams,
@@ -21,7 +21,7 @@ export class TipdialogComponent implements OnInit {
     this.did = this.navParams.get('did');
     this.feedName = this.navParams.get('name');
     this.feedDesc = this.navParams.get('des');
-    this.feedPublicStatus =  this.navParams.get('feedPublicStatus') || "";
+    this.feedPublicStatus =  this.navParams.get('feedPublicStatus') || true;
   }
 
   cancel(){
