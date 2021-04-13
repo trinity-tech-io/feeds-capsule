fa8929d6f006d60659ed7e9cc9867e968fe38e76
snackwang <snackwang.petter@gmail.com>
2021-04-13 17:37:04 +0800

    CU-7qwpwq add titlebar

---
+++ src/app/components/titlebarmenuitem/titlebarmenuitem.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { NavParams, PopoverController } from '@ionic/angular';
+import { TitleBarMenuItem } from '../titlebar/titlebar.types';
+
+@Component({
+  selector: 'app-titlebarmenuitem',
+  templateUrl: './titlebarmenuitem.component.html',
+  styleUrls: ['./titlebarmenuitem.component.scss'],
+})
+export class TitlebarmenuitemComponent implements OnInit {
+
+  public menuItems: TitleBarMenuItem[] = [];
+
+  constructor(
+    private navParams: NavParams,
+    private popoverCtrl: PopoverController
+  ) { }
+
+  ngOnInit() {
+    this.menuItems = this.navParams.get('items');
+  }
+
+  onTitlebarMenuItemClicked(item: TitleBarMenuItem) {
+    this.popoverCtrl.dismiss({
+      item: item
+    });
+  }
+
+  getIconPath(icon) {
+    return icon;
+  }
+
+}
