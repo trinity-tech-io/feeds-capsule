b7b523cf7d1a14823012e40b20e66dce5651718b
snackwang <snackwang.petter@gmail.com>
2021-04-27 22:42:51 +0800

    CU-81vc47 Add BarcodeScanner

---
+++ src/app/services/scan.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { BarcodeScanner, BarcodeScannerOptions,BarcodeScanResult } from '@ionic-native/barcode-scanner/ngx';
+
+@Injectable()
+export class ScanService {
+    constructor(private barcodeScanner: BarcodeScanner){
+    }
+
+    scanBarcode():Promise<string> {
+        return new Promise(async (resolve, reject) =>{
+            const options: BarcodeScannerOptions = {
+            preferFrontCamera: false,
+            showFlipCameraButton: false,
+            showTorchButton: false,
+            torchOn: false,
+            prompt: 'Place a qrCode inside the scan area',
+            resultDisplayDuration: 0,
+            formats: 'EAN_13,EAN_8,QR_CODE,PDF_417 ',
+            orientation: 'portrait',
+            };
+
+            let result:BarcodeScanResult = await this.barcodeScanner.scan(options);
+            resolve(result.text);
+        // this.barcodeScanner.scan(options).then(barcodeData => {
+        //   console.log('Barcode data', barcodeData);
+        // //   this.scannedData = barcodeData;
+    
+        // }).catch(err => {
+        //   console.log('Error', err);
+        // });
+        });
+    }
+    
+    createBarcode(input: string) {
+        this.barcodeScanner.encode(this.barcodeScanner.Encode.TEXT_TYPE, input).then((encodedData) => {
+            console.log(encodedData);
+        //   this.encodedData = encodedData;
+        }, (err) => {
+            console.log('Error occured : ' + err);
+        });
+    }
+}
