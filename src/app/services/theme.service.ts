f216d51dd80c81dd25c3134c1efb99f8dd2b9217
songjiazhun <869077613@qq.com>
2021-05-21 12:16:58 +0800

    CU-8kz5rf - add dark mode setting

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,46 +1,66 @@
-import { Injectable, ViewChild } from '@angular/core';
+import { Injectable} from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { theme } from "@elastosfoundation/elastos-connectivity-sdk-cordova";
-//import { TitleBarService } from 'src/app/services/TitleBarService';
-//import { TitleBarComponent } from 'src/app/components/titlebar/titlebar.component';
-
+import { ThemeDetection,ThemeDetectionResponse} from '@ionic-native/theme-detection';
 @Injectable({
   providedIn: 'root'
 })
 export class ThemeService {
-  //@ViewChild(TitleBarComponent, { static: true }) titleBar: TitleBarComponent;
-  public darkMode = false;
-
-  constructor(private platform: Platform) {
+  public darkMode = true;
+  constructor(
+    private platform: Platform) {
     this.platform.ready().then(() => {
       this.getTheme();
     });
   }
 
   getTheme() {
-    //TODO
-    // appManager.getPreference("ui.darkmode", (value) => {
-    //   this.darkMode = value;
-    //   this.setTheme(this.darkMode);
-    // });
+    let isDrakMode = localStorage.getItem('org.elastos.dapp.feeds.isDrakMode') || "";
+    if(isDrakMode === ""){
+      this.getSystemMode();
+     return;
+    }
+    this.setTheme(JSON.parse(isDrakMode));
   }
 
-  setTheme(dark) {
+  getSystemMode(){
+    ThemeDetection.isAvailable().then((res: ThemeDetectionResponse) => {
+      if(res.value) {
+        ThemeDetection.isDarkModeEnabled().then((darkModeRes: ThemeDetectionResponse) => {
+              if(darkModeRes.value){
+               this.darkMode = true;
+               this.setTheme(this.darkMode);
+              }else{
+               this.darkMode = false;
+               this.setTheme(this.darkMode);
+             }
+        })
+        .catch(() =>{
+         this.darkMode = false;
+         this.setTheme(this.darkMode);
+        }
+       );
+      }else{
+       this.darkMode = false;
+       this.setTheme(this.darkMode);
+      }
+   }).catch(
+     (error: any) => {
+      this.darkMode = false;
+      this.setTheme(this.darkMode);
+     }
+   );
+  }
+
+  setTheme(dark:boolean) {
     this.darkMode = dark;
     if (this.darkMode) {
       // Set dark mode globally
       document.body.classList.add("dark");
-
-      // Set dark mode to native header
-      //this.titleBarService.setBackgroundColor(this.titleBar, "#191a2f");
-      //this.titleBarService.setForegroundMode(this.titleBar, FeedsData.TitleBarForegroundMode.LIGHT);
     } else {
       // Remove dark mode globally
       document.body.classList.remove("dark");
-
-      // Remove dark mode to native header
-      //this.titleBarService.setBackgroundColor(this.titleBar, "#f8f8ff");
-      //this.titleBarService.setForegroundMode(this.titleBar, FeedsData.TitleBarForegroundMode.DARK);
     }
+
+    localStorage.setItem("org.elastos.dapp.feeds.isDrakMode",JSON.stringify(this.darkMode));
   }
 }
