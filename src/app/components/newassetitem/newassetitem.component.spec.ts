265a15ca9b5e7190436955a76a26fdd105d96e87
songjiazhun <869077613@qq.com>
2021-07-12 17:20:56 +0800

    CU-a616v8 - add nft list item style

---
+++ src/app/components/newassetitem/newassetitem.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { NewassetitemComponent } from './newassetitem.component';
+
+describe('NewassetitemComponent', () => {
+  let component: NewassetitemComponent;
+  let fixture: ComponentFixture<NewassetitemComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NewassetitemComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NewassetitemComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
