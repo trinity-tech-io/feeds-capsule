a70154a9110bbb356c6bb0b2edf23dca3fab7d43
songjiazhun <869077613@qq.com>
2021-08-10 14:37:02 +0800

    CU-cqpcd6 - fix post url dispaly style issue

--- src/app/pipes/html.pipe.ts
+++ src/app/pipes/html.pipe.ts
@@ -9,7 +9,7 @@ export class HtmlPipe implements PipeTransform {
   }
 
   replaceSrc(txt: string) {
-    let reg = /(((http[s]?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/gi;
+    let reg=/(http|https):\/\/[\w\-_]+(\.[\w\-_]+)+([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?/;
     let result = txt.replace(reg, function(item) {
       return "<span class='httpSpan'>" + item + '</span>';
     });
