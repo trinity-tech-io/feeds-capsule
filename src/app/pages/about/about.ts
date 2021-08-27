730d9772f5d6bf73a5d07a3c32cf44ed8a8f99a5
snackwang <snackwang.petter@gmail.com>
2021-08-27 14:16:30 +0800

    Update version to 2.0.2

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -15,7 +15,7 @@ import { TitleBarComponent } from 'src/app/components/titlebar/titlebar.componen
 export class AboutPage implements OnInit {
   @ViewChild(TitleBarComponent, { static: true }) titleBar: TitleBarComponent;
   public connectionStatus = 1;
-  public version = '2.0.1';
+  public version = '2.0.2';
   public currentLanguage = '';
 
   constructor(
