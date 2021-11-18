18e4b0ac62aa992c412d0570f0d87f4547b30dad
snackwang <snackwang.petter@gmail.com>
2021-11-18 14:08:13 +0800

    CU-1t8pr20 Fix the bug of easy crash when there is a large GIF in pasar

--- src/app/pages/feeds/profile/profile.page.ts
+++ src/app/pages/feeds/profile/profile.page.ts
@@ -968,7 +968,7 @@ export class ProfilePage implements OnInit {
            if(isload === ""){
              if (kind == 'gif' && size && parseInt(size, 10) > 10 * 1000 * 1000) {
                Logger.log(TAG, 'Work around, Not show');
-               return;
+               continue;
              }
 
              let fetchUrl = this.ipfsService.getNFTGetUrl() + fileName;
