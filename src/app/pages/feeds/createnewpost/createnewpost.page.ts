6508f8b6ddfc1671ea945ff0a11ce10deffd6129
songjiazhun <869077613@qq.com>
2022-03-02 13:05:45 +0800

    improve code

--- src/app/pages/feeds/createnewpost/createnewpost.page.ts
+++ src/app/pages/feeds/createnewpost/createnewpost.page.ts
@@ -278,6 +278,7 @@ export class CreatenewpostPage implements OnInit {
           this.backHome();
         } catch (error) {
           this.isLoading = false;
+          this.isPublishing = false;
         }
 
       }
