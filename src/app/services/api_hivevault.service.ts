dc5475523358b02dd2dcd4d9573e6f56f892fe33
snackwang <snackwang.petter@gmail.com>
2022-03-04 09:41:03 +0800

    WIP-hiveapi

--- src/app/services/api_hivevault.service.ts
+++ src/app/services/api_hivevault.service.ts
@@ -1,11 +1,85 @@
 import { Injectable } from '@angular/core';
+import { HiveService } from 'src/app/services/HiveService';
 
 const TAG = 'API-HiveVault';
 
 @Injectable()
 export class HiveVaultApi {
   constructor(
+    private hiveService: HiveService
   ) {
+  }
+
+  //API
+  //channel/post/comment/
+  createCollection(collectName: string) {
+    this.hiveService.createChannel(collectName);
+  }
+
+  //API
+  //Channel
+  //Insert
+  insertDataToChannelDB() {
+  }
+
+  //API
+  //Channel
+  //Update
+  updateDataToChannelDB() {
+  }
+
+  //API
+  //Post
+  //insert
+  insertDataToPostDB() {
+  }
+
+  //API
+  //Post
+  //Update
+  updateDataToPostDB() {
+  }
+
+  //API
+  //Post
+  //Delete
+  deleteDataFromPostDB() {
+  }
+
+  //API
+  //Post
+  //Register Get
+  registerGetPostScripting() {
+
+  }
+
+  //API
+  //Post
+  //Call Get
+  callGetPostScripting() {
+
+  }
+
+  //API
+  //Comment
+  //Update
+  insertDataToCommentDB() {
+  }
+
+
+  registerGetCommentScripting() {
+  }
+
+  callGetCommentScripting() {
+  }
+
+  createComment() {
+  }
+
+  updateComment() {
+  }
+
+  deleteComment() {
   }
 
 }
