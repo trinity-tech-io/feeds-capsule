0cd28408fa325342d290cbea370916d6f8aa9398
liaihong <>
2022-03-15 17:19:48 +0800

    CU-2dmyrv0 Optimize Hive service code and separate business logic

--- src/app/services/api_hivevault.service.ts
+++ src/app/services/api_hivevault.service.ts
@@ -1021,4 +1021,159 @@ export class HiveVaultApi {
     const executable = new FileDownloadExecutable(remoteName).setOutput(true);
     return this.hiveService.registerScript(HiveVaultApi.SCRIPT_CHANNEL, executable, null, false);
   }
+
+  async downloadEssAvatar() {
+    try {
+      // 检测本地是否存在
+      let userDid = (await this.dataHelper.getSigninData()).did
+      const loadKey = userDid + "_ess_avatar"
+      let essavatar = await this.dataHelper.loadUserAvatar(loadKey)
+      if (essavatar) {
+        return
+      }
+      const result = await this.hiveService.downloadEssAvatarTransactionId()
+      const transaction_id = result["download"]["transaction_id"]
+      let self = this
+      let dataBuffer = await this.hiveService.downloadScripting(transaction_id)
+
+      const savekey = userDid + "_ess_avatar"
+      const rawImage = await rawImageToBase64DataUrl(dataBuffer)
+      self.dataHelper.saveUserAvatar(savekey, rawImage)
+    }
+    catch (error) {
+      Logger.error(TAG, "Download Ess Avatar error: ", error);
+    }
+  }
+
+  async downloadCustomeAvatar(remotePath: string) {
+    try {
+      // 检测本地是否存在
+      let userDid = (await this.dataHelper.getSigninData()).did
+      let avatar = await this.dataHelper.loadUserAvatar(userDid);
+
+      if (avatar) {
+        return
+      }
+
+
+      let self = this
+      let imgstr = ''
+      try {
+        var dataBuffer = await this.hiveService.downloadFile(remotePath)
+        // dataBuffer = dataBuffer.slice(1, -1)
+        imgstr = dataBuffer.toString()
+        self.dataHelper.saveUserAvatar(userDid, imgstr)
+      } catch (error) {
+        Logger.error(TAG, 'Download custom avatar error: ', JSON.stringify(error))
+      }
+    } catch (error) {
+      Logger.error(TAG, 'Download error', error)
+    }
+  }
+}
+
+interface Mime {
+  mime: string;
+  pattern: (number | undefined)[];
+}
+
+const imageMimes: Mime[] = [
+  {
+    mime: 'image/png',
+    pattern: [0x89, 0x50, 0x4e, 0x47]
+  },
+  {
+    mime: 'image/jpeg',
+    pattern: [0xff, 0xd8, 0xff]
+  },
+  {
+    mime: 'image/gif',
+    pattern: [0x47, 0x49, 0x46, 0x38]
+  },
+  {
+    mime: 'image/webp',
+    pattern: [0x52, 0x49, 0x46, 0x46, undefined, undefined, undefined, undefined, 0x57, 0x45, 0x42, 0x50, 0x56, 0x50],
+  }
+];
+
+/**
+ * Encodes a raw binary picture data into a base64 string.
+ * Ex: âPNG   IHDR... ---> "iVe89...."
+ */
+export function rawImageToBase64(rawImageData: Buffer): string {
+  if (!rawImageData)
+    return null
+
+  return Buffer.from(rawImageData).toString("base64")
+}
+
+/**
+ * Converts a base64 encoded raw binary picture data into its original raw binary buffer.
+ * Ex: "iVe89...." ---> âPNG   IHDR...
+ */
+export function base64ImageToBuffer(base64Picture: string): Buffer {
+  return Buffer.from(base64Picture, "base64")
+}
+
+/**
+ * Converts a raw binary picture data to a base64 data url usable on UI.
+ * Ex: âPNG   IHDR... ---> "data:image/png;base64,iVe89...."
+ */
+export async function rawImageToBase64DataUrl(rawImageData: Buffer): Promise<string> {
+  if (!rawImageData)
+    return null;
+
+  let mimeType = await pictureMimeType(rawImageData)
+  if (!mimeType) {
+    Logger.warn("picturehelper", "Unable to extract mime type from picture buffer. rawImageToBase64DataUrl() returns null picture.")
+    return null
+  }
+
+  return "data:" + mimeType + ";base64," + rawImageToBase64(rawImageData)
+}
+
+/**
+ * Returns a 1x1 px fully transparent picture, encoded as base64 data url.
+ * Use https://png-pixel.com/ to generate.
+ */
+export function transparentPixelIconDataUrl(): string {
+  return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII='
+}
+
+function isMime(bytes: Uint8Array, mime: Mime): boolean {
+  return mime.pattern.every((p, i) => !p || bytes[i] === p)
+}
+
+/**
+ * @param rawOrBase64ImageData Raw picture buffer, or base64 encoded raw picture (not a base64 data url)
+ */
+export function pictureMimeType(rawOrBase64ImageData: Buffer | string): Promise<string> {
+  if (typeof rawOrBase64ImageData === "string")
+    rawOrBase64ImageData = base64ImageToBuffer(rawOrBase64ImageData)
+
+  const numBytesNeeded = Math.max(...imageMimes.map(m => m.pattern.length))
+  const blob = new Blob([rawOrBase64ImageData.slice(0, numBytesNeeded)]) // Read the needed bytes of the file
+
+  const fileReader = new FileReader()
+  let p = new Promise<string>((resolve) => {
+    fileReader.onloadend = e => {
+      if (!e || !fileReader.result) {
+        resolve(null)
+        return
+      }
+
+      const bytes = new Uint8Array(fileReader.result as ArrayBuffer)
+
+      const mime = imageMimes.find(mime => isMime(bytes, mime))
+
+      if (!mime)
+        resolve(null)
+      else
+        resolve(mime.mime)
+    };
+  });
+
+  fileReader.readAsArrayBuffer(blob)
+
+  return p
 }
