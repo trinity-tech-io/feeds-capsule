4812524f77677018e66bb249f3b01dd0d9c1dfb9
liaihong <>
2022-04-08 10:38:39 +0800

    add method for sqlite_hepler

--- src/app/services/sqlite_helper.service.ts
+++ src/app/services/sqlite_helper.service.ts
@@ -7,6 +7,10 @@ const TAG: string = 'sqlite-helper';
 @Injectable()
 export class FeedsSqliteHelper {
   private readonly TABLE_POST: string = 'poststest';
+  private readonly TABLE_CHANNEL: string = 'channeltest';
+  private readonly TABLE_COMMENT: string = 'commenttest';
+  private readonly TABLE_LIKE: string = 'liketest';
+
   private readonly LIMIT: number = 2;
 
   private sqliteObject: SQLiteObject = null;
@@ -75,6 +79,10 @@ export class FeedsSqliteHelper {
     return new Promise(async (resolve, reject) => {
       try {
         await this.cretePostTable();
+        await this.createChannelTable();
+        await this.createCommentTable();
+        await this.createLikeTable();
+
         resolve('SUCCESS');
       } catch (error) {
         Logger.error(TAG, 'Create table error', error);
@@ -83,7 +91,7 @@ export class FeedsSqliteHelper {
     });
   }
 
-
+// Post
   private cretePostTable(): Promise<any> {
     return new Promise(async (resolve, reject) => {
       try {
@@ -188,6 +196,255 @@ export class FeedsSqliteHelper {
 
   }
 
+  // channel
+  private createChannelTable(): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'create table ' + this.TABLE_CHANNEL
+          + '('
+          + 'dest_did VARCHAR(64), channel_id VARCHAR(64), channel_name VARCHAR(64), intro TEXT, created_at REAL(64), updated_at REAL(64),'
+          + 'avatarAddress TEXT, tippingAddress TEXT, type VARCHAR(64), proof VARCHAR(64),nft TEXT, memo TEXT, category TEXT'
+          + ')';
+        const result = await this.executeSql(statement);
+        console.log('creteChannelTable-------------------', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'Create channel table error', error);
+        reject(error);
+      }
+    });
+  }
+
+  insertChannelData(channelV3: FeedsData.ChannelV3): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'INSERT INTO ' + this.TABLE_CHANNEL
+          + '(dest_did, channel_id, channel_name, intro, created_at, updated_at, avatarAddress, tippingAddress, type, proof, nft, memo, category) VALUES'
+          + '(?,?,?,?,?,?,?,?,?,?,?,?,?)';
+
+        const params = [channelV3.destDid, channelV3.channelId, channelV3.name, channelV3.intro, channelV3.createdAt, channelV3.updatedAt
+          , JSON.stringify(channelV3.avatar), channelV3.tipping_address, channelV3.type, channelV3.proof, channelV3.nft, channelV3.memo, channelV3.category];
+
+        const result = await this.executeSql(statement, params);
+        Logger.log(TAG, 'Insert channel Data result is', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'Insert channel table date error', error);
+        reject(error);
+      }
+    });
+  }
+
+  queryChannelData(): Promise<FeedsData.ChannelV3[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'SELECT * FROM ' + this.TABLE_CHANNEL;
+        const result = await this.executeSql(statement);
+        const channelList = this.parseChannelData(result);
+        resolve(channelList);
+      } catch (error) {
+        Logger.error(TAG, 'query Channel Data error', error);
+        reject(error);
+      }
+    });
+  }
+
+  queryChannelDataByChannelId(channelId: string): Promise<FeedsData.ChannelV3[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'SELECT * FROM ' + this.TABLE_CHANNEL + ' WHERE channel_id=?';
+        const params = [channelId];
+        const result = await this.executeSql(statement, params);
+        const channelList = this.parseChannelData(result);
+
+        console.log('queryData-------------------', channelList);
+        resolve(channelList);
+      } catch (error) {
+        Logger.error(TAG, 'query Channel Data By ID  error', error);
+        reject(error);
+      }
+    });
+  }
+
+  queryMyChannel(userDid: string): Promise<FeedsData.ChannelV3[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'SELECT * FROM ' + this.TABLE_CHANNEL
+        const result = await this.executeSql(statement)
+        const channelList = this.parseChannelData(result)
+        let list = [];
+        channelList.forEach(channel => {
+          if (channel.destDid = userDid) list.push(channel)
+        })
+        console.log('queryData-------------------', channelList);
+        resolve(list);
+      } catch (error) {
+        Logger.error(TAG, 'query Channel Data By ID  error', error);
+        reject(error);
+      }
+    });
+  }
+
+  updateChannelData(channelV3: FeedsData.ChannelV3) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'UPDATE ' + this.TABLE_CHANNEL
+          + ' SET channel_name=?, intro=?, created_at=?, updated_at=?, avatarAddress=?, tippingAddress=?, type=?, proof=?, nft=?, memo=?, category=? WHERE channel_id=?';
+        const params = [channelV3.name, JSON.stringify(channelV3.intro), channelV3.createdAt, channelV3.updatedAt, channelV3.avatar, channelV3.tipping_address, channelV3.type, channelV3.proof, channelV3.nft, channelV3.memo, channelV3.category];
+
+        const result = await this.executeSql(statement, params);
+        Logger.log(TAG, 'update channel data result is', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'update channel data error', error);
+        reject(error);
+      }
+    });
+  }
+
+  // comment
+  private createCommentTable(): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'create table ' + this.TABLE_COMMENT
+          + '('
+          + 'dest_did VARCHAR(64), comment_id VARCHAR(64), channel_id VARCHAR(64), post_id VARCHAR(64), refcomment_id VARCHAR(64), content TEXT, created_at REAL(64)'
+          + ')';
+        const result = await this.executeSql(statement);
+        console.log('create Comment table-------------------', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'Create Comment table error', error);
+        reject(error);
+      }
+    });
+  }
+
+  insertCommentData(commentV3: FeedsData.CommentV3): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'INSERT INTO ' + this.TABLE_CHANNEL
+          + '(dest_did, comment_id, channel_id, post_id, refcomment_id, content, created_at) VALUES'
+          + '(?,?,?,?,?,?,?)';
+
+        const params = [commentV3.destDid, commentV3.commentId, commentV3.channelId, commentV3.postId, commentV3.refcommentId, JSON.stringify(commentV3.content)
+          , commentV3.createdAt];
+
+        const result = await this.executeSql(statement, params);
+        Logger.log(TAG, 'Insert comment Data result is', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'Insert comment table date error', error);
+        reject(error);
+      }
+    });
+  }
+
+  updateCommentData(commentV3: FeedsData.CommentV3) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'UPDATE ' + this.TABLE_CHANNEL
+          + ' SET content=? WHERE comment_id=?'; // 条件是否使用refcomment_id
+        const params = [commentV3.destDid, commentV3.commentId, commentV3.channelId, commentV3.postId, commentV3.refcommentId, JSON.stringify(commentV3.content), commentV3.createdAt];
+        + '(dest_did, comment_id, channel_id, post_id, refcomment_id, content, created_at) VALUES'
+
+        const result = await this.executeSql(statement, params);
+        Logger.log(TAG, 'update comment data result is', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'update comment data error', error);
+        reject(error);
+      }
+    });
+  }
+
+  queryCommentById(commentId: string): Promise<FeedsData.CommentV3[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'SELECT * FROM ' + this.TABLE_COMMENT + ' WHERE comment_id=?';
+        const params = [commentId];
+        const result = await this.executeSql(statement, params);
+        const commentList = this.parseCommentData(result);
+
+        console.log('queryData-------------------', commentList);
+        resolve(commentList);
+      } catch (error) {
+        Logger.error(TAG, 'query comment Data By ID  error', error);
+        reject(error);
+      }
+    });
+  }
+
+  queryCommentData(): Promise<FeedsData.CommentV3[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'SELECT * FROM ' + this.TABLE_COMMENT;
+        const result = await this.executeSql(statement);
+        const commentList = this.parseCommentData(result);
+        resolve(commentList);
+      } catch (error) {
+        Logger.error(TAG, 'query comment Data error', error);
+        reject(error);
+      }
+    });
+  }
+
+  // like 
+  private createLikeTable(): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'create table ' + this.TABLE_LIKE
+          + '('
+          + 'dest_did VARCHAR(64), channel_id VARCHAR(64), post_id VARCHAR(64), comment_id VARCHAR(64), created_at REAL(64)'
+          + ')';
+        const result = await this.executeSql(statement);
+        console.log('create like table-------------------', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'Create like table error', error);
+        reject(error);
+      }
+    });
+  }
+
+  insertLike(likeV3: FeedsData.LikeV3): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'INSERT INTO ' + this.TABLE_CHANNEL
+          + '(dest_did, channel_id, post_id, comment_id, created_at) VALUES'
+          + '(?,?,?,?,?)';
+        const params = [likeV3.destDid, likeV3.channelId, likeV3.postId, likeV3.commentId, likeV3.createdAt];
+
+        const result = await this.executeSql(statement, params);
+        Logger.log(TAG, 'Insert like result is', result);
+        resolve('SUCCESS');
+      } catch (error) {
+        Logger.error(TAG, 'Insert like table date error', error);
+        reject(error);
+      }
+    });
+  }
+
+  queryLikeData(): Promise<FeedsData.LikeV3[]> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const statement = 'SELECT * FROM ' + this.TABLE_LIKE;
+        const result = await this.executeSql(statement);
+        const likeList = this.parseLikeData(result);
+        resolve(likeList);
+      } catch (error) {
+        Logger.error(TAG, 'query comment Data error', error);
+        reject(error);
+      }
+    });
+  }
+
+  removeLike(likeV3: FeedsData.LikeV3): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+
+    });
+  }
+
   test() {
     let testdb: SQLiteObject;
     this.sqlite.create({
@@ -234,7 +491,6 @@ export class FeedsSqliteHelper {
     return list;
   }
 
-
   queryCommentNum(destDid: string, channelId: string, postId: string, commentId: string): Promise<number> {
     return new Promise(async (resolve, reject) => {
       try {
@@ -257,5 +513,37 @@ export class FeedsSqliteHelper {
         reject(error);
       }
     });
+  }
+  parseChannelData(result: any): FeedsData.ChannelV3[] {
+    Logger.log(TAG, 'Parse channel result from sql, result is', result);
+    let list = [];
+    for (let index = 0; index < result.rows.length; index++) {
+      const element = result.rows.item(index);
+      list.push(element);
+    }
+    Logger.log(TAG, 'Parse channel list from sql, list is', list);
+    return list;
+  }
+
+  parseCommentData(result: any): FeedsData.CommentV3[] {
+    Logger.log(TAG, 'Parse comment result from sql, result is', result);
+    let list = [];
+    for (let index = 0; index < result.rows.length; index++) {
+      const element = result.rows.item(index);
+      list.push(element);
+    }
+    Logger.log(TAG, 'Parse comment list from sql, list is', list);
+    return list;
+  }
+
+  parseLikeData(result: any): FeedsData.LikeV3[] {
+    Logger.log(TAG, 'Parse like result from sql, result is', result);
+    let list = [];
+    for (let index = 0; index < result.rows.length; index++) {
+      const element = result.rows.item(index);
+      list.push(element);
+    }
+    Logger.log(TAG, 'Parse like list from sql, list is', list);
+    return list;
   }
 }
